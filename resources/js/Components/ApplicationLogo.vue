89223a997f668bee0b493677ff260b10ef6e1b7c
Waqas Ali <waqas.ali@technologyplex.com>
2023-05-23 13:49:03 +0500

    initial commit

---
+++ resources/js/Components/ApplicationLogo.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="https://tplex.com/wp-content/uploads/2022/08/tplex-white-min.png" srcset="https://tplex.com/wp-content/uploads/2022/08/tplex-white-min.png 1x,  2x" alt="Offshore Software Development Company" class="logo logoDefault">
+</template>
